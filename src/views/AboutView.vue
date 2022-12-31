37c3f49c7cb99ca80f37cc55d59d4286e52194c7
飓枫 <112387102+HurricanMaple@users.noreply.github.com>
2022-12-31 19:04:48 +0800

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -8,32 +8,9 @@
       <br>
       
       <footer>(c) 2022 飓枫.  <a href="https://fiee.wtdown.top"> 博客</a> </footer>
-      <!-- require APlayer -->
 
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aplayer/dist/APlayer.min.css">
-
-<script src="https://cdn.jsdelivr.net/npm/aplayer/dist/APlayer.min.js"></script>
-
-<!-- require MetingJS -->
-
-<script src="https://cdn.jsdelivr.net/npm/meting@2/dist/Meting.min.js"></script>
-
-<meting-js
-
-    server="netease"
-
-    type="playlist"
-
-    id="7633921136">
-
-</meting-js>        
-
-<div style="text-align: center">
-
-    <p style="float:center;height:20px;line-height:20px;margin: 10px 0px 10px 0px;color:#737373;">Copyright © 2022 飓枫
-
-<script async src="https://npm.elemecdn.com/penndu@1.0.0/bsz.js"></script>
-        <span id="busuanzi_container_site_pv">本站总访问量<span id="busuanzi_value_site_pv"></span>次</span>
+      <script async src="https://npm.elemecdn.com/penndu@1.0.0/bsz.js"></script>
+      <span id="busuanzi_container_site_pv">本站总访问量<span id="busuanzi_value_site_pv"></span>次</span>
     </div>
   </div>
 </template>
